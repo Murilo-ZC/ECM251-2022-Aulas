a0c53c00c6b463264c6b40cad5bff823ea95f1d5
Murilo Zanini de Carvalho <murilo.carvalho@maua.br>
2022-08-18 10:54:06 -0300

    Criacao da classe Cartao

---
+++ Lab01/projeto-loja-online/modelos/pagamentos/cartoes.py
@@ -0,0 +1,12 @@
+class Cartao(Pagamento):
+    def __init__(self, numero, titular, validade, cvv):
+        self._numero = numero
+        self._titular = titular
+        self._cvv = cvv
+        self._validade = validade
+    def get_titular(self):
+        return self._titular
+    def get_validade(self):
+        return self._validade
+    def get_final(self):
+        return self._numero[len(self._numero)-4:]
