c33c401e1d774aa0b93a59cf675c72f9b947e41d
Murilo Zanini de Carvalho <murilo.carvalho@maua.br>
2022-08-18 12:31:05 -0300

    Adicionando perfil a classe Usuario

--- Lab02/projeto-lojinha-online/modelos/usuarios/usuario.py
+++ Lab02/projeto-lojinha-online/modelos/usuarios/usuario.py
@@ -1,8 +1,14 @@
+from perfil import Perfil
 class Usuario:
     def __init__(self, user_name, password, email):
         self._user_name = user_name
         self._password = password
         self._email = email
+        self._perfil = Perfil(nome=user_name)
+
+    def get_perfil(self):
+        return self._perfil
+
     def get_user_name(self):
         return self._user_name
     
