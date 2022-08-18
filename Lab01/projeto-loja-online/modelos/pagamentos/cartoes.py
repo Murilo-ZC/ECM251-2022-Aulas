a76a9179d9e9551f4ec926c0d1b2327275c3aebd
Murilo Zanini de Carvalho <murilo.carvalho@maua.br>
2022-08-18 10:58:08 -0300

    Criacao da classe cartao de Debito

--- Lab01/projeto-loja-online/modelos/pagamentos/cartoes.py
+++ Lab01/projeto-loja-online/modelos/pagamentos/cartoes.py
@@ -10,3 +10,9 @@ class Cartao(Pagamento):
         return self._validade
     def get_final(self):
         return self._numero[len(self._numero)-4:]
+
+class Debito(Cartao):
+    def __init__(self, numero, titular, validade, cvv):
+        super().__init__(numero, titular, validade, cvv)
+    def realizar_pagamento(self):
+        return True
